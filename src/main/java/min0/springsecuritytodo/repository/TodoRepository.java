0b9ba1acca7062eb2188b1ca98a83693637e5836
HongMinYeong <minyung1240@daum.net>
2023-12-08 15:35:29 +0900

    feat: todo repository 추가

---
+++ src/main/java/min0/springsecuritytodo/repository/TodoRepository.java
@@ -0,0 +1,14 @@
+package min0.springsecuritytodo.repository;
+
+import min0.springsecuritytodo.entity.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// TodoRepository 인터페이스
+// - jpaRepository 인터페이스를 확장한 인터페이스
+// - TodoEntity : 테이블에 매핑될 엔티티 클래스
+// - Long : 엔티티의 PK 타입
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity,Long> { //두번째에 pk 타입
+
+}
