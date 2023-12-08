69c6cb747fc0240c1632c1e5d9229dafe66d6773
HongMinYeong <minyung1240@daum.net>
2023-12-08 14:44:29 +0900

    Initial commit

---
+++ src/main/java/min0/springsecuritytodo/SpringSecurityTodoApplication.java
@@ -0,0 +1,13 @@
+package min0.springsecuritytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/min0/springsecuritytodo/SpringSecurityTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package min0.springsecuritytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
